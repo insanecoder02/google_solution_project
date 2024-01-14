0bdc3b39e54379234cd3e617168aa2316d10e65b
Ishant Kumawat <120731858+insanecoder02@users.noreply.github.com>
2024-01-14 21:04:22 +0530

    commit 3 ishant

--- lib/shop.dart
+++ lib/shop.dart
@@ -7,7 +7,7 @@ class shop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text('Page 1'),
+      child:Text('Page 1'),
     );
   }
 }
